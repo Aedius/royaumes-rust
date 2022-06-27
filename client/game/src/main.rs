9bfe2c63ddcbfa22699ec95185a7b4890fb0cf27
Aedius <aedius.filmania@gmail.com>
2022-06-27 11:46:23 +0200

    cargo fmt

--- client/game/src/main.rs
+++ client/game/src/main.rs
@@ -1,7 +1,6 @@
-use bevy::prelude::*;
-
 use std::collections::HashMap;
 
+use bevy::prelude::*;
 
 const GRAVITY_CONST: f32 = 0.0005;
 
@@ -16,9 +15,9 @@ fn main() {
 
 #[derive(Component)]
 struct Speed {
-    x : f32,
+    x: f32,
     y: f32,
-    w : f32,
+    w: f32,
 }
 
 fn setup(mut commands: Commands, _: Res<AssetServer>) {
@@ -34,7 +33,7 @@ fn setup(mut commands: Commands, _: Res<AssetServer>) {
             transform: Transform::from_xyz(100., -100., 0.),
             ..default()
         })
-        .insert(Speed{
+        .insert(Speed {
             x: 25.,
             y: 30.,
             w: 5000000.,
@@ -50,42 +49,47 @@ fn setup(mut commands: Commands, _: Res<AssetServer>) {
             transform: Transform::from_xyz(-100., 100., 0.),
             ..default()
         })
-        .insert(Speed{
+        .insert(Speed {
             x: -8.,
             y: -10.,
             w: 20000000.,
         });
 }
 
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(Entity, &mut Speed, &mut Transform)>) {
-
+fn sprite_movement(
+    time: Res<Time>,
+    mut sprite_position: Query<(Entity, &mut Speed, &mut Transform)>,
+) {
     let mut forces = HashMap::new();
 
-    for (e, speed, item) in sprite_position.iter(){
-        for (e2, speed2, item2) in sprite_position.iter(){
-            if e != e2{
-
-                let direction = (item2.translation.x - item.translation.x, item2.translation.y - item.translation.y);
-                let distance = ((item2.translation.x - item.translation.x).powi(2) + (item2.translation.y - item.translation.y).powi(2)).sqrt();
+    for (e, speed, item) in sprite_position.iter() {
+        for (e2, speed2, item2) in sprite_position.iter() {
+            if e != e2 {
+                let direction = (
+                    item2.translation.x - item.translation.x,
+                    item2.translation.y - item.translation.y,
+                );
+                let distance = ((item2.translation.x - item.translation.x).powi(2)
+                    + (item2.translation.y - item.translation.y).powi(2))
+                .sqrt();
 
                 let unit_direction = (direction.0 / distance, direction.1 / distance);
                 let force_scalar = GRAVITY_CONST * speed.w * speed2.w / distance.powi(2);
                 let acc_scalar = force_scalar / speed.w;
                 let acc_vector = (unit_direction.0 * acc_scalar, unit_direction.1 * acc_scalar);
 
-                let acc = forces.entry(e).or_insert((0.,0.));
-                *acc = (acc.0 + acc_vector.0, acc.1+ acc_vector.1);
+                let acc = forces.entry(e).or_insert((0., 0.));
+                *acc = (acc.0 + acc_vector.0, acc.1 + acc_vector.1);
             }
         }
     }
 
     for (e, mut speed, mut item) in sprite_position.iter_mut() {
-        item.translation.x += speed.x  * time.delta_seconds();
+        item.translation.x += speed.x * time.delta_seconds();
         item.translation.y += speed.y * time.delta_seconds();
 
-        let acc = forces.entry(e).or_insert((0.,0.));
+        let acc = forces.entry(e).or_insert((0., 0.));
         speed.x += acc.0;
         speed.y += acc.1;
     }
 }
-
